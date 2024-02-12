1b24d754a2a209315f6f2f6a7278cd45aade09e1
Mitchell Salomon <mitchell_salomon@brown.edu>
2024-02-12 16:44:46 -0500

    initial copy of essential amd64 setup from cs1660

---
+++ docker/container-setup-amd64.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set -eu
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
+target_user="${1:-cs1270-user}"
+
+export DEBIAN_FRONTEND=noninteractive
+export TZ=America/New_York
+
+# set up default locale
+export LANG=en_US.UTF-8
+
+apt-get update &&\
+  yes | unminimize
+
+# install GCC-related packages
+apt-get update && apt-get -y install\
+#  build-essential\
+#  binutils-doc\
+#  cpp-doc\
+#  gcc-doc\
+#  g++\
+#  g++-multilib\
+#  gdb\
+#  gdbserver\
+#  gdb-doc\
+#  glibc-doc\
+#  libblas-dev\
+#  liblapack-dev\
+#  liblapack-doc\
+#  libstdc++-11-doc\
+ make\
+ make-doc
+
+# Do main setup
+$SCRIPT_DIR/container-setup-common $target_user
+
+# Install golang
+bash -c "mkdir /usr/local/go && wget -O - https://go.dev/dl/go1.21.6.linux-amd64.tar.gz | tar -xvz -C /usr/local"
+
+# create binary reporting version of dockerfile
+(echo '#\!/bin/sh'; echo 'echo 1') > /usr/bin/cs1270-docker-version
+chmod ugo+rx,u+w,go-w /usr/bin/cs1270-docker-version
---
+++ docker/container-setup-common.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+set -eu
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
+target_user="${1:-cs1270-user}"
+
+# set up default locale
+export LANG=en_US.UTF-8
+
+# set up libraries
+# apt-get -y install\
+#  libreadline-dev\
+#  locales\
+#  wamerican\
+#  libssl-dev
+
+# set up default locale
+locale-gen en_US.UTF-8
+export LANG=en_US.UTF-8
+
+# install programs used for system exploration
+apt-get -y install\
+ blktrace\
+ linux-tools-generic\
+ strace\
+ tcpdump\
+ htop\
+ file\
+ xxd
+
+apt-get install -y python3 \
+	python3-pip \
+	python3-dev \
+	python3-setuptools \
+	python3-venv
+
+# install interactive programs (emacs, vim, nano, man, sudo, etc.)
+apt-get -y install\
+ bc\
+ curl\
+ dc\
+ git\
+ git-doc\
+ man\
+ micro\
+ nano\
+ psmisc\
+ sudo\
+ wget\
+ screen\
+ tmux\
+ emacs-nox\
+ vim
+
+# install programs used for networking
+# apt-get -y install\
+#  dnsutils\
+#  inetutils-ping\
+#  iproute2\
+#  net-tools\
+#  netcat\
+#  nmap\
+#  telnet\
+#  time\
+#  pv\
+#  traceroute\
+#  tshark
+
+# remove unneeded .deb files
+rm -r /var/lib/apt/lists/*
+
+# Set up the container user
+if [[ $target_user == "cs1270-user" ]]; then
+    useradd -m -s /bin/bash $target_user
+else
+    # If using the host's user, don't create one--podman will do this
+    # automatically.  However, the default shell will be wrong, so set
+    # a profile rule to update this
+    chmod +x /etc/profile.d/20-fix-default-shell.sh # Copied in Podmanfile
+fi
+
+# set up passwordless sudo for user cs1270-user
+echo "cs1270-user ALL=(ALL:ALL) NOPASSWD: ALL" > /etc/sudoers.d/cs1270-init
+
+rm -f /root/.bash_logout
